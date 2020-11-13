a693a9234b5708ceb5f9d8eda1c74db07cb87d2c
Shubanker Chourasia <subhankerchourasia@gmail.com>
2020-11-14 00:19:18 +0530

    reafctor

--- Daily Coding Problem/713/713 shortest-path.ts
+++ Daily Coding Problem/713/713 shortest-path.ts
@@ -4,15 +4,17 @@ Given an absolute pathname that may have . or .. as part of it, return the short
 For example, given "/usr/bin/../bin/./scripts/../", return "/usr/bin/".
  */
 export const shortestFilePath = (absolutePath: string) => {
-  const shortDirs = [];
-  absolutePath.split("/").forEach((dir) => {
-    if (dir === "..") {
-      shortDirs.pop();
-    } else if (dir !== ".") {
-      shortDirs.push(dir);
-    }
-  });
-  return shortDirs.join("/");
+  return absolutePath
+    .split("/")
+    .reduce((accum, dir) => {
+      if (dir === "..") {
+        accum.pop();
+      } else if (dir !== ".") {
+        accum.push(dir);
+      }
+      return accum;
+    }, [])
+    .join("/");
 };
 console.log(shortestFilePath("/usr/bin/../bin/./scripts/../"));
 console.log(shortestFilePath("/usr/bin/../bin/./scripts/../abc/../shub/./"));
