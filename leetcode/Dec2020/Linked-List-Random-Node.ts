2578fed832e92ada2a82dfbab31179c461f791ed
Shubanker Chourasia <subhankerchourasia@gmail.com>
2020-12-02 20:28:23 +0530

    [LC] Linked List Random Node

---
+++ leetcode/Dec2020/Linked-List-Random-Node.ts
@@ -0,0 +1,56 @@
+/**
+Linked List Random Node
+Given a singly linked list, return a random node's value from the linked list. Each node must have the same probability of being chosen.
+
+Follow up:
+What if the linked list is extremely large and its length is unknown to you? Could you solve this efficiently without using extra space?
+
+Example:
+
+// Init a singly linked list [1,2,3].
+ListNode head = new ListNode(1);
+head.next = new ListNode(2);
+head.next.next = new ListNode(3);
+Solution solution = new Solution(head);
+
+// getRandom() should return either 1, 2, or 3 randomly. Each element should have equal probability of returning.
+solution.getRandom();
+ */
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+class Solution {
+  constructor(private head: ListNode | null) {}
+  private getRandomNo(min: number, max: number) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  getRandom(): number {
+    let iterator = this.head;
+    let result: number,
+      i = 1;
+    while (iterator) {
+      if (this.getRandomNo(1, i++) === 1) {
+        result = iterator.val;
+      }
+      iterator = iterator.next;
+    }
+    return result;
+  }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * var obj = new Solution(head)
+ * var param_1 = obj.getRandom()
+ */
