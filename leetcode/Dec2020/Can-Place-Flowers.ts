594d3b6a29148e46920c7fb3e578e3d7636d8962
Shubanker Chourasia <subhankerchourasia@gmail.com>
2020-12-05 14:51:45 +0530

    [LC] Can Place Flowers

---
+++ leetcode/Dec2020/Can-Place-Flowers.ts
@@ -0,0 +1,37 @@
+/**
+Can Place Flowers
+You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots.
+
+Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, return if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule.
+
+ 
+
+Example 1:
+
+Input: flowerbed = [1,0,0,0,1], n = 1
+Output: true
+Example 2:
+
+Input: flowerbed = [1,0,0,0,1], n = 2
+Output: false
+ 
+
+Constraints:
+
+1 <= flowerbed.length <= 2 * 104
+flowerbed[i] is 0 or 1.
+There are no two adjacent flowers in flowerbed.
+0 <= n <= flowerbed.length
+ */
+function canPlaceFlowers(flowerbed: number[], n: number): boolean {
+  for (let i = 0; i < flowerbed.length && n > 0; i++) {
+    if (flowerbed[i] === 0) {
+      if ((i === 0 || flowerbed[i - 1] === 0) && (i === flowerbed.length - 1 || flowerbed[i + 1] === 0)) {
+        n--;
+        flowerbed[i++] = 1;
+      }
+    }
+  }
+  return n === 0;
+}
+console.log(canPlaceFlowers([0, 0, 1, 0, 0], 1));
