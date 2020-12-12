2eb90a5ce00abc13a42ba8e4de60d501b9230942
Shubanker Chourasia <subhankerchourasia@gmail.com>
2020-12-12 19:51:01 +0530

    [LC] Smallest Subtree with all the Deepest Nodes added tests

--- leetcode/Dec2020/Smallest-Subtree-with-all-the-Deepest-Nodes.ts
+++ leetcode/Dec2020/Smallest-Subtree-with-all-the-Deepest-Nodes.ts
@@ -78,4 +78,39 @@ The values of the nodes in the tree are unique.
     }
     return Math.max(getHeight(root.left), getHeight(root.right)) + 1;
   };
+
+  //Utils
+  const arrayToTree = (ar: number[]) => {
+    const root = new TreeNode(ar[0]);
+    let leafNodes: TreeNode[] = [root];
+    const getNode = (number: number) => {
+      if (number === null) {
+        return null;
+      }
+      return new TreeNode(number);
+    };
+    for (let index = 1; index < ar.length; ) {
+      let nextLeafes = [];
+      leafNodes.forEach((leaf) => {
+        if (index < ar.length) {
+          leaf.left = getNode(ar[index]);
+        }
+        if (index + 1 < ar.length) {
+          leaf.right = getNode(ar[index + 1]);
+        }
+        index += 2;
+        if (leaf.left) {
+          nextLeafes.push(leaf.left);
+        }
+        if (leaf.right) {
+          nextLeafes.push(leaf.right);
+        }
+      });
+      leafNodes = nextLeafes;
+    }
+    return root;
+  };
+
+  //Test
+  console.log(subtreeWithAllDeepest(arrayToTree([3, 5, 1, 6, 2, 0, 8, null, null, 7, 4])));
 }
