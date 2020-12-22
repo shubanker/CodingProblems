c6a4167101b4c3a891fe8eae5b48b50c26c15d8f
Shubanker Chourasia <subhankerchourasia@gmail.com>
2020-12-22 19:27:46 +0530

    Balanced Binary Tree

---
+++ leetcode/Dec2020/Balanced-Binary-Tree.ts
@@ -0,0 +1,78 @@
+/**
+Balanced Binary Tree
+Given a binary tree, determine if it is height-balanced.
+
+For this problem, a height-balanced binary tree is defined as:
+
+a binary tree in which the left and right subtrees of every node differ in height by no more than 1.
+
+ 
+
+Example 1:
+
+
+Input: root = [3,9,20,null,null,15,7]
+Output: true
+Example 2:
+
+
+Input: root = [1,2,2,3,3,null,null,4,4]
+Output: false
+Example 3:
+
+Input: root = []
+Output: true
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 5000].
+-104 <= Node.val <= 104
+ */
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+{
+  class TreeNode {
+    val: number;
+    left: TreeNode | null;
+    right: TreeNode | null;
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+      this.val = val === undefined ? 0 : val;
+      this.left = left === undefined ? null : left;
+      this.right = right === undefined ? null : right;
+    }
+  }
+  function isBalanced(root: TreeNode | null): boolean {
+    return checkBalanced(root, new Map());
+  }
+  const checkBalanced = (root: TreeNode | null, memo: Map<TreeNode, number>) => {
+    if (root === null) {
+      return true;
+    }
+    if (Math.abs(getHeight(root.left, memo) - getHeight(root.right, memo)) > 1) {
+      return false;
+    }
+    return checkBalanced(root.left, memo) && checkBalanced(root.right, memo);
+  };
+  const getHeight = (root: TreeNode | null, memo: Map<TreeNode, number>) => {
+    if (memo.has(root)) {
+      return memo.get(root);
+    }
+    if (root === null) {
+      return 0;
+    }
+    memo.set(root, 1 + Math.max(getHeight(root.left, memo), getHeight(root.right, memo)));
+    return memo.get(root);
+  };
+}
