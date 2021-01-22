2e83db9796a07e3109b94fcfc5051478ba4dcf20
Shubanker Chourasia <subhankerchourasia@gmail.com>
2021-01-22 21:02:15 +0530

    [LC] Determine if Two Strings Are Close

---
+++ leetcode/Jan2021/Determine-if-Two-Strings-Are-Close.ts
@@ -0,0 +1,68 @@
+/**
+Determine if Two Strings Are Close
+Two strings are considered close if you can attain one from the other using the following operations:
+
+Operation 1: Swap any two existing characters.
+For example, abcde -> aecdb
+Operation 2: Transform every occurrence of one existing character into another existing character, and do the same with the other character.
+For example, aacabb -> bbcbaa (all a's turn into b's, and all b's turn into a's)
+You can use the operations on either string as many times as necessary.
+
+Given two strings, word1 and word2, return true if word1 and word2 are close, and false otherwise.
+
+ 
+
+Example 1:
+
+Input: word1 = "abc", word2 = "bca"
+Output: true
+Explanation: You can attain word2 from word1 in 2 operations.
+Apply Operation 1: "abc" -> "acb"
+Apply Operation 1: "acb" -> "bca"
+Example 2:
+
+Input: word1 = "a", word2 = "aa"
+Output: false
+Explanation: It is impossible to attain word2 from word1, or vice versa, in any number of operations.
+Example 3:
+
+Input: word1 = "cabbba", word2 = "abbccc"
+Output: true
+Explanation: You can attain word2 from word1 in 3 operations.
+Apply Operation 1: "cabbba" -> "caabbb"
+Apply Operation 2: "caabbb" -> "baaccc"
+Apply Operation 2: "baaccc" -> "abbccc"
+Example 4:
+
+Input: word1 = "cabbba", word2 = "aabbss"
+Output: false
+Explanation: It is impossible to attain word2 from word1, or vice versa, in any amount of operations.
+ 
+
+Constraints:
+
+1 <= word1.length, word2.length <= 105
+word1 and word2 contain only lowercase English letters.
+ */
+
+function closeStrings(word1: string, word2: string): boolean {
+  if (word1.length !== word2.length) {
+    return false;
+  }
+  const freqCount1: Record<string, number> = Object.create(null);
+  const freqCount2: Record<string, number> = Object.create(null);
+  const charSet = new Set<string>();
+  for (let index = 0; index < word1.length; index++) {
+    const char = word1.charAt(index);
+    charSet.add(char);
+    freqCount1[char] = (freqCount1[char] || 0) + 1;
+  }
+  for (let index = 0; index < word2.length; index++) {
+    const char = word2.charAt(index);
+    if (!charSet.has(char)) {
+      return false;
+    }
+    freqCount2[char] = (freqCount2[char] || 0) + 1;
+  }
+  return Object.values(freqCount1).sort().join() == Object.values(freqCount2).sort().join();
+}
