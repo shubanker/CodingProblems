dfc4f35c563cf4acbe30ba040e92d00b115d1c45
Shubanker Chourasia <subhankerchourasia@gmail.com>
2021-02-17 18:11:38 +0530

    [LC] Container With Most Water

---
+++ leetcode/Feb2021/Container-With-Most-Water.ts
@@ -0,0 +1,49 @@
+/**
+Container With Most Water
+Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai). n vertical lines are drawn such that the two endpoints of the line i is at (i, ai) and (i, 0). Find two lines, which, together with the x-axis forms a container, such that the container contains the most water.
+
+Notice that you may not slant the container.
+
+ 
+
+Example 1:
+
+
+Input: height = [1,8,6,2,5,4,8,3,7]
+Output: 49
+Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+Example 2:
+
+Input: height = [1,1]
+Output: 1
+Example 3:
+
+Input: height = [4,3,2,1,4]
+Output: 16
+Example 4:
+
+Input: height = [1,2,1]
+Output: 2
+ 
+
+Constraints:
+
+n == height.length
+2 <= n <= 3 * 104
+0 <= height[i] <= 3 * 104
+ */
+function maxArea(height: number[]): number {
+  let max = 0;
+  let front = 0,
+    tail = height.length - 1;
+  while (front < tail) {
+    const minHeight = Math.min(height[front], height[tail]);
+    max = Math.max(max, minHeight * (tail - front));
+    if (height[front] === minHeight) {
+      front++;
+    } else {
+      tail--;
+    }
+  }
+  return max;
+}
