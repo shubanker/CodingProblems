3ee70e6f628b53b9e9e00097d425cccf56611e80
Shubanker Chourasia <subhankerchourasia@gmail.com>
2021-03-05 18:02:31 +0530

    [LC] Average of Levels in Binary Tree

---
+++ leetcode/Mar2021/Average-of-Levels-in-Binary-Tree.ts
@@ -0,0 +1,59 @@
+/**
+Average of Levels in Binary Tree
+Given a non-empty binary tree, return the average value of the nodes on each level in the form of an array.
+Example 1:
+Input:
+    3
+   / \
+  9  20
+    /  \
+   15   7
+Output: [3, 14.5, 11]
+Explanation:
+The average value of nodes on level 0 is 3,  on level 1 is 14.5, and on level 2 is 11. Hence return [3, 14.5, 11].
+Note:
+The range of node's value is in the range of 32-bit signed integer.
+ */
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+function averageOfLevels(root: TreeNode | null): number[] {
+  const op: number[] = [];
+  if (!root) {
+    return op;
+  }
+  let stack = [root];
+  while (stack.length) {
+    let sum = 0;
+    const nextStack: TreeNode[] = [];
+    stack.forEach((node) => {
+      sum += node.val;
+
+      node.left && nextStack.push(node.left);
+      node.right && nextStack.push(node.right);
+    });
+    op.push(sum / stack.length);
+    stack = nextStack;
+  }
+  return op;
+}
