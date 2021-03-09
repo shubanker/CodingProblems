e6db7d0d08a8295e795955d59546dbd56d063270
Shubanker Chourasia <subhankerchourasia@gmail.com>
2021-03-09 22:21:11 +0530

    [LC] Add One Row to Tree

---
+++ leetcode/Mar2021/Add-One-Row-to-Tree.ts
@@ -0,0 +1,97 @@
+/**
+Add One Row to Tree
+
+Solution
+Given the root of a binary tree, then value v and depth d, you need to add a row of nodes with value v at the given depth d. The root node is at depth 1.
+
+The adding rule is: given a positive integer depth d, for each NOT null tree nodes N in depth d-1, create two tree nodes with value v as N's left subtree root and right subtree root. And N's original left subtree should be the left subtree of the new left subtree root, its original right subtree should be the right subtree of the new right subtree root. If depth d is 1 that means there is no depth d-1 at all, then create a tree node with value v as the new root of the whole original tree, and the original tree is the new root's left subtree.
+
+Example 1:
+Input: 
+A binary tree as following:
+       4
+     /   \
+    2     6
+   / \   / 
+  3   1 5   
+
+v = 1
+
+d = 2
+
+Output: 
+       4
+      / \
+     1   1
+    /     \
+   2       6
+  / \     / 
+ 3   1   5   
+
+Example 2:
+Input: 
+A binary tree as following:
+      4
+     /   
+    2    
+   / \   
+  3   1    
+
+v = 1
+
+d = 3
+
+Output: 
+      4
+     /   
+    2
+   / \    
+  1   1
+ /     \  
+3       1
+Note:
+The given d is in range [1, maximum depth of the given tree + 1].
+The given binary tree has at least one tree node.
+ */
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function addOneRow(root: TreeNode | null, v: number, d: number): TreeNode | null {
+  if (!root) {
+    return root;
+  }
+  if (d === 1) {
+    return new TreeNode(v, root);
+  }
+  let stack = [root];
+  while (stack.length && d > 2) {
+    const nextStack: TreeNode[] = [];
+    stack.forEach((node) => {
+      node.left && nextStack.push(node.left);
+      node.right && nextStack.push(node.right);
+    });
+    d--;
+    stack = nextStack;
+  }
+
+  if (d === 2) {
+    stack.forEach((node) => {
+      console.log(node.val);
+      const { left, right } = node;
+      node.left = new TreeNode(v, left);
+      node.right = new TreeNode(v, void 0, right);
+    });
+  }
+  return root;
+}
