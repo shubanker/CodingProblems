9baf2a1cdc6a7d18b8bb1983ea6a5d62dcd8a9e9
Shubanker Chourasia <subhankerchourasia@gmail.com>
2021-03-27 13:17:48 +0530

    [LC] Palindromic Substrings

---
+++ leetcode/Mar2021/Palindromic-Substrings.ts
@@ -0,0 +1,39 @@
+/**
+Palindromic Substrings
+Given a string, your task is to count how many palindromic substrings in this string.
+
+The substrings with different start indexes or end indexes are counted as different substrings even they consist of same characters.
+
+Example 1:
+
+Input: "abc"
+Output: 3
+Explanation: Three palindromic strings: "a", "b", "c".
+ 
+
+Example 2:
+
+Input: "aaa"
+Output: 6
+Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
+ 
+
+Note:
+
+The input string length won't exceed 1000.
+
+ */
+function countSubstrings(s: string): number {
+  let count = 0;
+  for (let i = 0; i < s.length; i++) {
+    count += countPanins(s, i) + countPanins(s, i, i + 1);
+  }
+  return count;
+}
+const countPanins = (str: string, left: number, right = left) => {
+  let count = 0;
+  while (str[left] && str[left--] === str[right++]) {
+    count++;
+  }
+  return count;
+};
