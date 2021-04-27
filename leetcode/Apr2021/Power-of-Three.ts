78c8147903e95118ac33f6fefbca9c5703545837
Shubanker Chourasia <subhankerchourasia@gmail.com>
2021-04-27 21:01:16 +0530

    [LC] Power of Three

---
+++ leetcode/Apr2021/Power-of-Three.ts
@@ -0,0 +1,49 @@
+/**
+Power of Three
+
+Solution
+Given an integer n, return true if it is a power of three. Otherwise, return false.
+
+An integer n is a power of three, if there exists an integer x such that n == 3x.
+
+ 
+
+Example 1:
+
+Input: n = 27
+Output: true
+Example 2:
+
+Input: n = 0
+Output: false
+Example 3:
+
+Input: n = 9
+Output: true
+Example 4:
+
+Input: n = 45
+Output: false
+ 
+
+Constraints:
+
+-231 <= n <= 231 - 1
+ 
+
+Follow up: Could you solve it without loops/recursion?
+ */
+function isPowerOfThree(n: number): boolean {
+  const threeBase = n.toString(3);
+  // return /^10*$/.test(threeBase);
+  //Manual Check
+  if (threeBase[0] == "1") {
+    for (let i = 1; i < threeBase.length; i++) {
+      if (threeBase[i] != "0") {
+        return false;
+      }
+    }
+    return true;
+  }
+  return false;
+}
