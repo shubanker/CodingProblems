10006ad1aaa83c183f9c37f76c26fceeb3630656
Shubanker Chourasia <subhankerchourasia@gmail.com>
2021-07-26 20:45:50 +0530

    [LC] Convert Sorted Array to Binary Search Tree

---
+++ leetcode/July2021/Convert-Sorted-Array-to-Binary-Search-Tree.ts
@@ -0,0 +1,56 @@
+import { TreeNode } from "../_includes/treeNode";
+/**
+Convert Sorted Array to Binary Search Tree
+
+Given an integer array nums where the elements are sorted in ascending order, convert it to a height-balanced binary search tree.
+
+A height-balanced binary tree is a binary tree in which the depth of the two subtrees of every node never differs by more than one.
+
+ 
+
+Example 1:
+
+
+Input: nums = [-10,-3,0,5,9]
+Output: [0,-3,9,-10,null,5]
+Explanation: [0,-10,5,null,-3,null,9] is also accepted:
+
+Example 2:
+
+
+Input: nums = [1,3]
+Output: [3,1]
+Explanation: [1,3] and [3,1] are both a height-balanced BSTs.
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+-104 <= nums[i] <= 104
+nums is sorted in a strictly increasing order.
+ */
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function sortedArrayToBST(nums: number[]): TreeNode | null {
+  if (nums == null || !nums.length) {
+    return null;
+  }
+
+  let mid = Math.floor(nums.length / 2);
+  const node = new TreeNode(nums[mid]);
+  node.left = sortedArrayToBST(nums.slice(0, mid));
+  node.right = sortedArrayToBST(nums.slice(mid + 1, nums.length));
+  return node;
+}
