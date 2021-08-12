775efc7b475e9f77a8328c611667f001c4369f9c
Shubanker Chourasia <subhankerchourasia@gmail.com>
2021-08-12 21:24:48 +0530

    [LC] Group Anagrams

---
+++ leetcode/Aug2021/Group-Anagrams.ts
@@ -0,0 +1,39 @@
+/**
+Group Anagrams
+Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+ 
+
+Example 1:
+
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+Example 2:
+
+Input: strs = [""]
+Output: [[""]]
+Example 3:
+
+Input: strs = ["a"]
+Output: [["a"]]
+ 
+
+Constraints:
+
+1 <= strs.length <= 104
+0 <= strs[i].length <= 100
+strs[i] consists of lower-case English letters.
+ */
+function groupAnagrams(strs: string[]): string[][] {
+  let map = new Map();
+  for (let s of strs) {
+    let key = s.split("").sort().join("");
+    if (!map.has(key)) {
+      map.set(key, []);
+    }
+    map.get(key).push(s);
+  }
+  return Array.from(map.values());
+}
