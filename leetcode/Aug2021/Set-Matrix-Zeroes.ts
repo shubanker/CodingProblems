df29fe23d395292c5283630bcbe773ff6dffc810
Shubanker Chourasia <subhankerchourasia@gmail.com>
2021-08-13 20:35:44 +0530

    [LC] Set Matrix Zeroes

---
+++ leetcode/Aug2021/Set-Matrix-Zeroes.ts
@@ -0,0 +1,64 @@
+/**
+Set Matrix Zeroes
+Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's, and return the matrix.
+
+You must do it in place.
+
+ 
+
+Example 1:
+
+
+Input: matrix = [[1,1,1],[1,0,1],[1,1,1]]
+Output: [[1,0,1],[0,0,0],[1,0,1]]
+Example 2:
+
+
+Input: matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+Output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
+ 
+
+Constraints:
+
+m == matrix.length
+n == matrix[0].length
+1 <= m, n <= 200
+-231 <= matrix[i][j] <= 231 - 1
+ 
+
+Follow up:
+
+A straightforward solution using O(mn) space is probably a bad idea.
+A simple improvement uses O(m + n) space, but still not the best solution.
+Could you devise a constant space solution?
+ */
+/**
+ Do not return anything, modify matrix in-place instead.
+ */
+function setZeroes(matrix: number[][]): void {
+  const rows = new Set<number>();
+  const cols = new Set<number>();
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[0].length; j++) {
+      if (matrix[i][j] === 0) {
+        rows.add(i);
+        cols.add(j);
+      }
+    }
+  }
+  rows.forEach((i) => {
+    matrix[i].fill(0);
+  });
+  cols.forEach((j) => {
+    for (let i = 0; i < matrix.length; i++) {
+      matrix[i][j] = 0;
+    }
+  });
+}
+console.log(
+  setZeroes([
+    [0, 1, 2, 0],
+    [3, 4, 5, 2],
+    [1, 3, 1, 5],
+  ])
+);
