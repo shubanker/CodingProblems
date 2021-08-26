0c088861400a544e26c27d712f04c8cf9e3b17ab
Shubanker Chourasia <subhankerchourasia@gmail.com>
2021-08-26 16:37:28 +0530

    [LC] Verify Preorder Serialization of a Binary Tree

---
+++ leetcode/Aug2021/Verify-Preorder-Serialization-of-a-Binary-Tree.ts
@@ -0,0 +1,67 @@
+/**
+Verify Preorder Serialization of a Binary Tree
+One way to serialize a binary tree is to use preorder traversal. When we encounter a non-null node, we record the node's value. If it is a null node, we record using a sentinel value such as '#'.
+
+
+For example, the above binary tree can be serialized to the string "9,3,4,#,#,1,#,#,2,#,6,#,#", where '#' represents a null node.
+
+Given a string of comma-separated values preorder, return true if it is a correct preorder traversal serialization of a binary tree.
+
+It is guaranteed that each comma-separated value in the string must be either an integer or a character '#' representing null pointer.
+
+You may assume that the input format is always valid.
+
+For example, it could never contain two consecutive commas, such as "1,,3".
+Note: You are not allowed to reconstruct the tree.
+
+ 
+
+Example 1:
+
+Input: preorder = "9,3,4,#,#,1,#,#,2,#,6,#,#"
+Output: true
+Example 2:
+
+Input: preorder = "1,#"
+Output: false
+Example 3:
+
+Input: preorder = "9,#,#,1"
+Output: false
+ 
+
+Constraints:
+
+1 <= preorder.length <= 104
+preoder consist of integers in the range [0, 100] and '#' separated by commas ','.
+ */
+function isValidSerialization(preorder: string): boolean {
+  if (preorder === "#") {
+    return true;
+  }
+
+  const ar = preorder.split(",");
+  const testValidForNode = (nodeIndex: number) => {
+    if (nodeIndex > ar.length || ar[nodeIndex] === "#") {
+      return -1;
+    }
+
+    let nextIndex = nodeIndex + 1;
+    if (ar[nextIndex] !== "#") {
+      nextIndex = testValidForNode(nextIndex);
+    } else {
+      nextIndex++;
+    }
+    if (nextIndex === -1) {
+      return -1;
+    }
+    if (ar[nextIndex] !== "#") {
+      return testValidForNode(nextIndex);
+    }
+    return nextIndex + 1;
+  };
+  const index = testValidForNode(0);
+  return index === ar.length;
+}
+console.log(isValidSerialization("9,3,4,#,#,1,#,#,2,#,6,#,#"));
+// console.log(isValidSerialization("9,#,92,#,#"));
