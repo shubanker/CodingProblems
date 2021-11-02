9d72a78ca755515b64548498cc36cb22f8fb518b
Shubanker Chourasia <subhankerchourasia@gmail.com>
2021-11-02 18:21:20 +0530

    Unique Paths III

---
+++ leetcode/Nov2021/Unique-Paths-III.ts
@@ -0,0 +1,89 @@
+/**
+Unique Paths III
+You are given an m x n integer array grid where grid[i][j] could be:
+
+1 representing the starting square. There is exactly one starting square.
+2 representing the ending square. There is exactly one ending square.
+0 representing empty squares we can walk over.
+-1 representing obstacles that we cannot walk over.
+Return the number of 4-directional walks from the starting square to the ending square, that walk over every non-obstacle square exactly once.
+
+ 
+
+Example 1:
+
+
+Input: grid = [[1,0,0,0],[0,0,0,0],[0,0,2,-1]]
+Output: 2
+Explanation: We have the following two paths: 
+1. (0,0),(0,1),(0,2),(0,3),(1,3),(1,2),(1,1),(1,0),(2,0),(2,1),(2,2)
+2. (0,0),(1,0),(2,0),(2,1),(1,1),(0,1),(0,2),(0,3),(1,3),(1,2),(2,2)
+Example 2:
+
+
+Input: grid = [[1,0,0,0],[0,0,0,0],[0,0,0,2]]
+Output: 4
+Explanation: We have the following four paths: 
+1. (0,0),(0,1),(0,2),(0,3),(1,3),(1,2),(1,1),(1,0),(2,0),(2,1),(2,2),(2,3)
+2. (0,0),(0,1),(1,1),(1,0),(2,0),(2,1),(2,2),(1,2),(0,2),(0,3),(1,3),(2,3)
+3. (0,0),(1,0),(2,0),(2,1),(2,2),(1,2),(1,1),(0,1),(0,2),(0,3),(1,3),(2,3)
+4. (0,0),(1,0),(2,0),(2,1),(1,1),(0,1),(0,2),(0,3),(1,3),(1,2),(2,2),(2,3)
+Example 3:
+
+
+Input: grid = [[0,1],[2,0]]
+Output: 0
+Explanation: There is no path that walks over every empty square exactly once.
+Note that the starting and ending square can be anywhere in the grid.
+ 
+
+Constraints:
+
+m == grid.length
+n == grid[i].length
+1 <= m, n <= 20
+1 <= m * n <= 20
+-1 <= grid[i][j] <= 2
+There is exactly one starting cell and one ending cell.
+ */
+function uniquePathsIII(grid: number[][]): number {
+  const m = grid.length;
+  const n = grid[0].length;
+  let zeros = 1;
+  const start = [-1, -1];
+  let res = 0;
+  const directions = [
+    [-1, 0],
+    [0, 1],
+    [1, 0],
+    [0, -1],
+  ];
+
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (grid[i][j] === 0) zeros++;
+      else if (grid[i][j] === 1) {
+        start[0] = i;
+        start[1] = j;
+      }
+    }
+  }
+
+  const dfs = (i: number, j: number) => {
+    if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] < 0) return;
+
+    if (grid[i][j] === 2) return (res += Number(!zeros));
+
+    grid[i][j] = -2;
+    zeros--;
+
+    directions.forEach(([ni, nj]) => dfs(i + ni, j + nj));
+
+    grid[i][j] = 0;
+    zeros++;
+  };
+
+  dfs(start[0], start[1]);
+
+  return res;
+}
