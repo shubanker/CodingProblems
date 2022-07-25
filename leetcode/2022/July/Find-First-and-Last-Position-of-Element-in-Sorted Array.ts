c8235035119bb51299a2bd968aa5fb57787dc079
Shubanker Chourasia <subhankerchourasia@gmail.com>
2022-07-25 18:03:22 +0530

    better approach.

--- leetcode/2022/July/Find-First-and-Last-Position-of-Element-in-Sorted Array.ts
+++ leetcode/2022/July/Find-First-and-Last-Position-of-Element-in-Sorted Array.ts
@@ -29,7 +29,7 @@ Constraints:
 nums is a non-decreasing array.
 -10^9 <= target <= 10^9
  */
-function searchRange(nums: number[], target: number): number[] {
+function searchRange_(nums: number[], target: number): number[] {
   const pos = [-1, -1];
   let left = 0,
     right = nums.length - 1;
@@ -59,5 +59,48 @@ function searchRange(nums: number[], target: number): number[] {
     }
   }
   return pos;
+}
+// True O(log n) sol
+function searchRange(nums: number[], target: number): number[] {
+  let left: number = 0;
+  let right: number = nums.length - 1;
+
+  const range: number[] = [-1, -1];
+  while (left <= right) {
+    const middle: number = Math.floor(left + (right - left) / 2);
+
+    if (target === nums[middle]) {
+      if ((nums[middle - 1] ?? -Infinity) !== target) range[0] = middle;
+      else range[0] = binaryHelper(nums, left, middle - 1, target, "left");
+
+      if ((nums[middle + 1] ?? Infinity) !== target) range[1] = middle;
+      else range[1] = binaryHelper(nums, middle + 1, right, target, "right");
+      return range;
+    }
+    if (target < nums[middle]) right = middle - 1;
+    else left = middle + 1;
+  }
+
+  return range;
+}
+
+function binaryHelper(nums: number[], left: number, right: number, target: number, edge: "left" | "right"): number {
+  while (left <= right) {
+    const middle: number = Math.floor(left + (right - left) / 2);
+
+    if (target === nums[middle]) {
+      if (edge === "left") {
+        if ((nums[middle - 1] ?? Infinity) !== target) return middle;
+        else right = middle - 1;
+      }
+      if (edge === "right") {
+        if ((nums[middle + 1] ?? -Infinity) !== target) return middle;
+        else left = middle + 1;
+      }
+    } else if (target < nums[middle]) right = middle - 1;
+    else left = middle + 1;
+  }
+
+  return -1;
 }
 console.log(searchRange([5, 7, 7, 8, 8, 10], 8));
