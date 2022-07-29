310192ad4716dd4dd0188d8d099d49ec47cbc0db
Shubanker Chourasia <subhankerchourasia@gmail.com>
2022-07-30 00:41:58 +0530

    Find N Unique Integers Sum up to Zero

---
+++ leetcode/2022/July/Find-N-Unique-Integers-Sum-up-to-Zero.ts
@@ -0,0 +1,35 @@
+/**
+Find N Unique Integers Sum up to Zero
+Given an integer n, return any array containing n unique integers such that they add up to 0.
+
+ 
+
+Example 1:
+
+Input: n = 5
+Output: [-7,-1,1,3,4]
+Explanation: These arrays also are accepted [-5,-1,1,2,3] , [-3,-1,2,-2,4].
+Example 2:
+
+Input: n = 3
+Output: [-1,0,1]
+Example 3:
+
+Input: n = 1
+Output: [0]
+ 
+
+Constraints:
+
+1 <= n <= 1000
+ */
+function sumZero(n: number): number[] {
+  const nums = [];
+  for (let i = 1; i <= n / 2; i++) {
+    nums.push(i, -i);
+  }
+  if (n % 2 === 1) {
+    nums.push(0);
+  }
+  return nums;
+}
