818b97f51258da5b74321a868cca2977e8a8592b
Shubanker Chourasia <subhankerchourasia@gmail.com>
2022-08-16 19:42:08 +0530

    First Unique Character in a String

---
+++ leetcode/2022/August/First-Unique-Character-in-a-String.ts
@@ -0,0 +1,41 @@
+/**
+First Unique Character in a String
+Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+
+ 
+
+Example 1:
+
+Input: s = "leetcode"
+Output: 0
+Example 2:
+
+Input: s = "loveleetcode"
+Output: 2
+Example 3:
+
+Input: s = "aabb"
+Output: -1
+ 
+
+Constraints:
+
+1 <= s.length <= 10^5
+s consists of only lowercase English letters.
+ */
+function firstUniqChar(s: string): number {
+  let m = new Map<string, number[]>();
+  for (let i = 0; i < s.length; i++) {
+    if (m.has(s[i])) {
+      m.get(s[i])[1] = 1;
+    } else {
+      m.set(s[i], [i, 0]);
+    }
+  }
+  for (const ar of m.values()) {
+    if (ar[1] === 0) {
+      return ar[0];
+    }
+  }
+  return -1;
+}
