20aff2693d728f17299130b0f3f1a11bcce4435d
Shubanker Chourasia <subhankerchourasia@gmail.com>
2024-05-21 14:59:19 +0530

    Subsets

---
+++ leetcode/2024/May/Subsets.ts
@@ -0,0 +1,37 @@
+/**
+78. Subsets
+
+Given an integer array nums of unique elements, return all possible subsets (the power set).
+
+The solution set must not contain duplicate subsets. Return the solution in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3]
+Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+Example 2:
+
+Input: nums = [0]
+Output: [[],[0]]
+ 
+
+Constraints:
+
+1 <= nums.length <= 10
+-10 <= nums[i] <= 10
+All the numbers of nums are unique.
+ */
+function subsets(nums: number[]): number[][] {
+  const op: number[][] = [[]];
+
+  for (let i = 0; i < nums.length; i++) {
+    op.forEach((item) => {
+      // bad practice to push in same array while iterating, but it works :)
+      op.push([...item, nums[i]]);
+    });
+  }
+  console.timeLog("t");
+  return op;
+}
